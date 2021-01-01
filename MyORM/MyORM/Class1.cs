3e93d09a60c87a59bd72f3652f767f87dcd50936
Trương Văn Tú <37862349+tumeotu@users.noreply.github.com>
2021-01-01 12:34:51 +0700

    Xóa bin obj

--- MyORM/MyORM/Class1.cs
+++ MyORM/MyORM/Class1.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using MyORM.SQLBuilder;
+using System;
 
 namespace MyORM
 {
 	public class Class1
 	{
+        static void main(string[] args)
+        {
+            SinhVien sinhVien = new SinhVien();
+            sinhVien.name = "sdsd";
+            sinhVien.DTB = 305;
+            SqlString<SinhVien> sqlString = new SqlString<SinhVien>();
+
+            sqlString.Update(sinhVien).Where(sv => sv.name == "sff");
+            Console.WriteLine(sqlString.sql);
+
+            sqlString.save(sinhVien);
+            Console.WriteLine(sqlString.sql);
+
+            sqlString.SelectAll().Where(sv => sv.name == "ss dfd").AND(sv => sv.DTB >= 5);
+            Console.WriteLine(sqlString.sql);
+        }
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ORM
 A framework is a ORM. It allow hanlde with databases
+
+Link markdown: https://hackmd.io/@Njnxp-EdS_KOJ623CouRNg/S1zKvsN6P/edit
