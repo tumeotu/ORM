22c037ce963b0d315e3d9edb02ded0e0e37b3b80
mrjared16 <doannguyentanphuc99@gmail.com>
2021-01-01 17:22:40 +0700

    add mapper attribute class to define database field in table

---
+++ MyORM/MyORM/Mapper/MapperAttribute/ColumnAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyORM.Mapper.MapperAttribute
+{
+    public class ColumnAttribute : Attribute
+    {
+        public string ColumnName;
+        private ColumnAttribute(string columnName)
+        {
+            this.ColumnName = columnName;
+        }
+    }
+}
---
+++ MyORM/MyORM/Mapper/MapperAttribute/TableAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyORM.Mapper.MapperAttribute
+{
+    public class TableAttribute : Attribute
+    {
+        public string TableName;
+        private TableAttribute(string name)
+        {
+            TableName = name;
+        }
+    }
+
+}
