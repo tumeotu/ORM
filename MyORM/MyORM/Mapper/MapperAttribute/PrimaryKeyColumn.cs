08f750c1f0a54e32086b4542be736bebb69b1e4c
mrjared16 <doannguyentanphuc99@gmail.com>
2021-01-05 06:43:30 +0700

    add primary key attribute

---
+++ MyORM/MyORM/Mapper/MapperAttribute/PrimaryKeyColumn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyORM.Mapper.MapperAttribute
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class PrimaryKeyColumn : Attribute
+    {
+        public string ColumnName;
+        private PrimaryKeyColumn(string columnName)
+        {
+            this.ColumnName = columnName;
+        }
+    }
+}
