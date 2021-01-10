e6690760d58d640e7e5d93dbf38136cafa27ecf4
mrjared16 <doannguyentanphuc99@gmail.com>
2021-01-10 14:34:26 +0700

    decouple mapping implement from mapping rule

---
+++ MyORM/MyORM/Mapper/MappingEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace MyORM.Mapper
+{
+    public abstract class MappingEngine
+    {
+        public abstract void Execute<T>(DataRow record, T result, string propertyName, HashSet<string> fieldsName, string resultAlias) where T : class, new();
+
+        private static Dictionary<MappingEngineType, MappingEngine> engines = new Dictionary<MappingEngineType, MappingEngine>();
+        static MappingEngine()
+        {
+            engines.Add(MappingEngineType.TableField, new TableFieldEngine());
+        }
+
+        public static MappingEngine GetEngineFromName(MappingEngineType type)
+        {
+            if (!engines.ContainsKey(type))
+            {
+                return null;
+            }
+            return engines[type];
+        }
+    }
+    public enum MappingEngineType
+    {
+        TableField
+    }
+}
