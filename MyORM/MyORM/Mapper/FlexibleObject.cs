dfdbf73f9dee3f0ede755754e536957a03cd3719
mrjared16 <doannguyentanphuc99@gmail.com>
2021-01-10 14:35:23 +0700

    add mapping column to entity property through wrapper object class

---
+++ MyORM/MyORM/Mapper/FlexibleObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace MyORM.Mapper
+{
+    public class FlexibleObject<T> where T : class, new()
+    {
+        private T core;
+
+        public FlexibleObject(T core)
+        {
+            this.core = core;
+        }
+
+        public void set(string propertyName, object targetValue)
+        {
+            PropertyInfo prop = core.GetType().GetProperty("Name", BindingFlags.Public | BindingFlags.Instance);
+            if (null != prop && prop.CanWrite)
+            {
+                prop.SetValue(core, targetValue, null);
+            }
+        }
+
+        public Type getType(string propertyName)
+        {
+            return core.GetType().GetProperty(propertyName)?.GetType();
+        }
+    }
+}
---
+++ MyORM/MyORM/Mapper/TableFieldEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace MyORM.Mapper
+{
+    class TableFieldEngine : MappingEngine
+    {
+        public override void Execute<T>(DataRow record, T result, string propertyName, HashSet<string> fieldsName, string resultAlias)
+        {
+            FlexibleObject<T> targetObject = new FlexibleObject<T>(result);
+            foreach (var field in fieldsName)
+            {
+                string columnName = resultAlias + "." + field;
+                var targetValue = Convert.ChangeType(record[columnName], targetObject.getType(propertyName));
+                targetObject.set(propertyName, targetValue);
+            }
+        }
+    }
+}
