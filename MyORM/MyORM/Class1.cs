54d546ab375a3f9e7d7948c5588a643bae3b41c0
mrjared16 <doannguyentanphuc99@gmail.com>
2021-01-10 17:12:17 +0700

    add class to test

--- MyORM/MyORM/Class1.cs
+++ MyORM/MyORM/Class1.cs
@@ -1,25 +1,50 @@
-﻿using MyORM.SQLBuilder;
+﻿using MyORM.Mapper;
+using Npgsql;
 using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace MyORM
 {
-	public class Class1
-	{
-        static void main(string[] args)
+    public class Class1
+    {
+        static void Main(string[] args)
         {
-            SinhVien sinhVien = new SinhVien();
-            sinhVien.name = "sdsd";
-            sinhVien.DTB = 305;
-            SqlString<SinhVien> sqlString = new SqlString<SinhVien>();
+            //SinhVien sinhVien = new SinhVien();
+            //sinhVien.name = "sdsd";
+            //sinhVien.DTB = 305;
+            //SqlString<SinhVien> sqlString = new SqlString<SinhVien>();
 
-            sqlString.Update(sinhVien).Where(sv => sv.name == "sff");
-            Console.WriteLine(sqlString.sql);
+            //sqlString.Update(sinhVien).Where(sv => sv.name == "sff");
+            //Console.WriteLine(sqlString.sql);
 
-            sqlString.save(sinhVien);
-            Console.WriteLine(sqlString.sql);
+            //sqlString.save(sinhVien);
+            //Console.WriteLine(sqlString.sql);
 
-            sqlString.SelectAll().Where(sv => sv.name == "ss dfd").AND(sv => sv.DTB >= 5);
-            Console.WriteLine(sqlString.sql);
+            //sqlString.SelectAll().Where(sv => sv.name == "ss dfd").AND(sv => sv.DTB >= 5);
+            //Console.WriteLine(sqlString.sql);
+            var table = new DataTable();
+            string query = "SELECT sinhvien.id as \"sinhvien.id\", sinhvien.name as \"sinhvien.name\", sinhvien.gpa as \"sinhvien.gpa\"  FROM sinhvien";
+            string connectionString = "Server=127.0.0.1;Port=5432;Database=gomoku-online;User Id=postgres;Password=1;";
+            using (var da = new NpgsqlDataAdapter(query, connectionString))
+            {
+                da.Fill(table);
+                foreach (DataColumn col in table.Columns)
+                {
+                    Console.WriteLine(col.ColumnName);
+                }
+                foreach (DataRow row in table.Rows)
+                {
+                    Console.WriteLine(row["sinhvien.id"].ToString() + " - " + row["sinhvien.name"].ToString() + " - " + row["sinhvien.gpa"].ToString());
+                }
+                DataMapper dataMapper = new DataMapper();
+                List<SinhVien> sv = dataMapper.loadAll<SinhVien>(table);
+                foreach (var sinhvien in sv)
+                {
+                    Console.WriteLine(sinhvien.ID + " - " + sinhvien.name + " - " + sinhvien.DTB);
+                }
+            }
+
         }
-	}
+    }
 }
--- MyORM/MyORM/SinhVien.cs
+++ MyORM/MyORM/SinhVien.cs
@@ -1,13 +1,18 @@
-﻿using System;
+﻿using MyORM.Mapper.MapperAttribute;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MyORM.SQLBuilder
+namespace MyORM
 {
+    [TableAttribute("sinhvien")]
     class SinhVien
     {
+        [PrimaryKeyColumn("id")]
         public int ID { get; set; }
+        [Column("name")]
         public string name { get; set; }
+        [Column("gpa")]
         public double DTB { get; set; }
         public SinhVien(string name, int ID, double dtb)
         {
